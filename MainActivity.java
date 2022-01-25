9c1a08a69f6c05ba9515230ad0cdbee7b4dff278
Renan Da Silva Souza <61859849+Renan3622@users.noreply.github.com>
2022-01-24 21:37:01 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.mycompany.edge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+import com.mycompany.edge.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       
+            WebView containerWebview = (WebView) findViewById(R.id.containerWebView);
+					containerWebview.setWebViewClient(new MyWebViewClient());
+					String url = "http://www.bing.com";
+					containerWebview.getSettings().setJavaScriptEnabled(true);
+					containerWebview.loadUrl(url); // load a web page in a web view
+				  
+				}
+				
+				private class MyWebViewClient extends WebViewClient {
+							@Override
+							public boolean shouldOverrideUrlLoading(WebView view, String url) {
+											view.loadUrl(url);
+											return true;
+					}
+				}
+
+    }
